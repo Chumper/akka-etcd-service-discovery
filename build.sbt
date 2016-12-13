2abf4ae554e34806a856b408c321c6368277064e
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-13 14:21:47 +0100

    commit all

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "akka-etcd-service-discovery"
 version := "1.0"
 
 scalaVersion := "2.12.0"
+
+lazy val etcd = RootProject(uri("git://github.com/Chumper/etcd3-scala"))
+
+dependsOn(etcd)
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.14"
--- src/main/scala/com.github.chumper/ServiceRegistryActor.scala
+++ src/main/scala/com.github.chumper/ServiceRegistryActor.scala
@@ -1,8 +1,21 @@
 package com.github.chumper
 
+import akka.actor.{Actor, Props}
+import io.grpc.stub.AbstractStub
+
 /**
   * Will hold a up to date list of requested services if they can be found in the registry
   */
-class ServiceRegistryActor {
+class ServiceRegistryActor(stub: AbstractStub[AbstractStub]) extends Actor {
 
+  stub.getChannel.
+
+
+  override def receive: Receive = {
+    case _ =>
+  }
+}
+
+object ServiceRegistryActor {
+  def props() = Props(new ServiceRegistryActor())
 }
