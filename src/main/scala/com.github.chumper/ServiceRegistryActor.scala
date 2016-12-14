ba52b2986a35ac3cfb3e784ee868fac630eb6da4
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-14 13:07:03 +0100

    upgraded scala version #2

--- src/main/scala/com.github.chumper/ServiceRegistryActor.scala
+++ src/main/scala/com.github.chumper/ServiceRegistryActor.scala
@@ -1,11 +1,12 @@
 package com.github.chumper
 
 import com.github.chumper.etcd.Etcd
+import akka.actor.Actor
 
 /**
   * Will hold a up to date list of requested services if they can be found in the registry
   */
-class ServiceRegistryActor(etcd: Etcd, serviceName: String, port: Int) extends Actor {
+class ServiceRegistryActor(etcd: Etcd, serviceName: String, port: Int){
 
 
 }
