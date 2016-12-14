aa737dcd14fdb0eee74ffb54cb37eb89af742890
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-14 11:50:42 +0100

    fine... take it

--- src/main/scala/com.github.chumper/ServiceRegistryActor.scala
+++ src/main/scala/com.github.chumper/ServiceRegistryActor.scala
@@ -1,11 +1,9 @@
 package com.github.chumper
 
-import io.grpc.stub.AbstractStub
-
 /**
   * Will hold a up to date list of requested services if they can be found in the registry
   */
-class ServiceRegistryActor(stub: AbstractStub[AbstractStub]) {
+class ServiceRegistryActor() {
 }
 
 object ServiceRegistryActor {
