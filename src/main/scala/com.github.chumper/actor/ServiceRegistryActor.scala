7f13fce956e74a7f470172e75d9b6f86dbd28113
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-15 10:55:35 +0100

    added lease to etcd

--- src/main/scala/com.github.chumper/actor/ServiceRegistryActor.scala
+++ src/main/scala/com.github.chumper/actor/ServiceRegistryActor.scala
@@ -1,17 +1,43 @@
 package com.github.chumper.actor
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, Cancellable, Props}
+import com.github.chumper.actor.ServiceRegistryActor.UpdateLease
 import com.github.chumper.etcd.Etcd
+
+import scala.concurrent.duration.DurationDouble
 
 /**
   * Will hold a up to date list of requested services if they can be found in the registry
   */
 class ServiceRegistryActor(etcd: Etcd, serviceName: String, port: Int) extends Actor {
-  override def receive: Receive = {
+  import context.dispatcher
 
+  // schedule a keep alive for 5 seconds
+  val tick: Cancellable = context.system.scheduler.schedule(5 seconds, 5 seconds, self, UpdateLease)
+
+  def updateLease(): Unit = {
+    // update lease
+  }
+
+  override def receive: Receive = {
+    // we are not acting on any custom commands except the lease update
+    case UpdateLease => updateLease()
+    case _ =>
+  }
+
+  override def preStart(): Unit = {
+    // insert key, add lease for 10 seconds
+  }
+
+  override def postStop(): Unit = {
+    // cancel periodic scheduling
+    tick.cancel()
   }
 }
 
 object ServiceRegistryActor {
+
+  sealed private case class UpdateLease() // used to update the lease of the registration
+
   def props(etcd: Etcd, serviceName: String, port: Int) = Props { new ServiceRegistryActor(etcd, serviceName, port) }
 }
