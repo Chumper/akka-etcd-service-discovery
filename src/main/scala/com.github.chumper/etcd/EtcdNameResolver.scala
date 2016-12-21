bb6ef9d0fe6eab4bd0d991c2694b307ca69d137a
n.plaschke <n.plaschke@dsfishlabs.com>
2016-12-21 14:47:30 +0100

    added service discovery for client

--- src/main/scala/com.github.chumper/etcd/EtcdNameResolver.scala
+++ src/main/scala/com.github.chumper/etcd/EtcdNameResolver.scala
@@ -1,21 +1,50 @@
 package com.github.chumper.etcd
 
+import java.net.{InetSocketAddress, SocketAddress}
+import java.util.Collections
+
 import akka.actor.ActorSystem
-import io.grpc.NameResolver
+import com.github.chumper.registry.EtcdRegistry
+import io.grpc.{Attributes, NameResolver, ResolvedServerInfo}
 import io.grpc.NameResolver.Listener
+
+import scala.collection.JavaConverters.seqAsJavaListConverter
 
 class EtcdNameResolver(serviceName: String)(implicit actorSystem: ActorSystem, etcd: Etcd) extends NameResolver {
 
+  var watchId: Option[Long] = None
+
   override def shutdown(): Unit = {
     // stop watching the keys
+    watchId match {
+      case Some(id) => etcd.watch.cancel(id)
+        watchId = None
+      case None =>
+    }
   }
 
-  override def getServiceAuthority: String = ???
+  override def getServiceAuthority: String = serviceName
 
   override def start(listener: Listener): Unit = {
-    // watch the keys
-  }
 
+    def getServers = etcd.kv.prefix(s"${EtcdRegistry.PREFIX}$serviceName").map { resp =>
+      // generate service list
+      val addresses = resp.kvs.map { _.value.toStringUtf8 }.map { e =>
+        val ad = e.split(":")
+        new ResolvedServerInfo(new InetSocketAddress(ad(0), ad(1).asInstanceOf[Int]), Attributes.EMPTY)
+      }
+      listener.onUpdate(Collections.singletonList(addresses.asJava), Attributes.EMPTY)
+    }
+
+    // get the keys
+    getServers
+
+    // watch the keys
+    etcd.watch.prefix(s"${EtcdRegistry.PREFIX}$serviceName") { resp =>
+      // list updated, so get the new servers
+      getServers
+    }
+  }
 }
 
 object EtcdNameResolver {
